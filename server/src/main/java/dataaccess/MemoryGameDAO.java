926a5c283b5e88e1c843d1679932da55a4a5f2d5
gavinturley <77turley@gmail.com>
2026-07-16 05:10:50 -0600

    Memory DAOs created and implemented. Used Atomic integer to generate unique IDs for games so it can handle multiple threads.

---
+++ server/src/main/java/dataaccess/MemoryGameDAO.java
@@ -0,0 +1,41 @@
+package dataaccess;
+
+import model.GameData;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.atomic.AtomicInteger;
+
+public class MemoryGameDAO implements GameDAO{
+    private final Map<Integer, GameData> games = new HashMap<>();
+    private final AtomicInteger nextID = new AtomicInteger(1);
+
+    @Override
+    public int createGame(GameData game){
+        int id = nextID.getAndIncrement();
+        GameData withID = new GameData(id, game.whiteUsername(), game.blackUsername(), game.gameName(), game.game());
+
+        games.put(id, withID);
+        return id;
+    }
+
+    @Override
+    public GameData getGame(int id){
+        return games.get(id);
+    }
+
+    @Override
+    public Collection<GameData> listGames(){
+        return games.values();
+    }
+
+    @Override
+    public void updateGame(GameData game){
+        games.put(game.gameID(), game);
+    }
+
+    @Override
+    public void clear(){
+        games.clear();
+    }
+}
