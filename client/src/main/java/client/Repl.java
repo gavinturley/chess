0ca1dbae16ae0a6fc846627e37c28366de2755d0
gavinturley <77turley@gmail.com>
2026-07-29 19:31:08 -0600

    Finished relp loop logic and added a new method to determine whether to use post login or pre login.

--- client/src/main/java/client/Repl.java
+++ client/src/main/java/client/Repl.java
@@ -1,4 +1,71 @@
 package client;
 
+import java.util.Scanner;
+import ui.EscapeSequences.SET_TEXT_COLOR_GREEN;
+import ui.EscapeSequences.RESET_TEXT_COLOR;
+
+import static ui.EscapeSequences.RESET_TEXT_COLOR;
+
+
 public class Repl {
+    private final PreLoginClient preLoginClient;
+    private final PostLoginClient postLoginClient;
+
+    private State state = State.SIGNED_OUT;
+    private String authToken = null;
+    private String username = null;
+
+    public Repl(String serverURL) {
+        ServerFacade facade = new ServerFacade(serverURL);
+        this.preLoginClient = new PreLoginClient(facade, this);
+        this.postLoginClient = new PostLoginClient(facade, this);
+    }
+
+    public void run(){
+        System.out.println(SET_TEXT_COLOR_GREEN + "♕ Welcome to 240 Chess. Type Help to get started. ♕" + RESET_TEXT_COLOR);
+        System.out.println(preLoginClient.eval("help"));
+
+        Scanner scanner = new Scanner(System.in);
+        var result = "";
+        while (result.equals("quit")){
+            printPrompt();
+            String line = scanner.nextLine();
+            try {
+                result = state == State.SIGNED_OUT ? preLoginClient.eval(line) : postLoginClient.eval(line);
+                System.out.print(result);
+            } catch (Throwable exception) {
+                System.out.print("Error: " + exception.getMessage());
+            }
+        }
+        System.out.println();
+    }
+
+    public void printPrompt(){
+        String label = state == State.SIGNED_OUT ? "SIGNED_OUT" : "SIGNED_IN";
+        System.out.print("\n" + RESET_TEXT_COLOR + "[" + label + "] >>> ");
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getAuthToken(){
+        return authToken;
+    }
+
+    public void setAuthToken(String authToken){
+        this.authToken = authToken;
+    }
+
+    public State getState(){
+        return state;
+    }
+
+    public void setState(State state){
+        this.state = state;
+    }
 }
